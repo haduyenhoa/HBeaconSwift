a914337d15d1c95dd497ea0cc8ec7de04118bcc7
Ha Duyen Hoa <tyt_g207@yahoo.com>
2014-06-10 13:47:19 +0200

    Create project

---
+++ swiftHBeacon/swiftHBeacon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  swiftHBeacon
+//
+//  Created by Duyen Hoa Ha on 10/06/2014.
+//  Copyright (c) 2014 Rayonnance Technologies. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
